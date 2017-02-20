e0635e8eacb9207566180c0e9f0bf15151893964
Mafia hsu <g8071107@gmail.com>
2017-02-20 20:24:12 +0800

    備份好習慣

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Jumbotron Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+    <link href="assets/style.css" rel="stylesheet">
+
+
+  </head>
+
+  <body>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <p>您好，請選擇商品分類：</p>
+         <div class="row">
+            <div class="col-sm-12 col-md-4" id="slider-thumbs">
+              <select class="form-control shop-category">
+                <option>清潔用品</option>
+                <option>廚房用品</option>
+              </select>
+            </div>
+          </div>
+      </div>
+    </div>
+
+
+    <div class="container shop-item">
+        <div id="main_area">
+            <!-- Slider -->
+            <div class="row">
+                <div class="col-sm-12" id="slider-thumbs">
+                    <!-- Bottom switcher of slider -->
+                    <ul class="hide-bullets">
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-0">
+                                <img src="http://placehold.it/255x152&text=zero">
+                                <p>123</p>
+                            </a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-1"><img src="http://placehold.it/255x152&text=1"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-2"><img src="http://placehold.it/255x152&text=2"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-3"><img src="http://placehold.it/255x152&text=3"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-4"><img src="http://placehold.it/255x152&text=4"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-5"><img src="http://placehold.it/255x152&text=5"></a>
+                        </li>
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-6"><img src="http://placehold.it/255x152&text=6"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-7"><img src="http://placehold.it/255x152&text=7"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-8"><img src="http://placehold.it/255x152&text=8"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-9"><img src="http://placehold.it/255x152&text=9"></a>
+                        </li>
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-10"><img src="http://placehold.it/255x152&text=10"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-11"><img src="http://placehold.it/255x152&text=11"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-12"><img src="http://placehold.it/255x152&text=12"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-13"><img src="http://placehold.it/255x152&text=13"></a>
+                        </li>
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-14"><img src="http://placehold.it/255x152&text=14"></a>
+                        </li>
+
+                        <li class="col-sm-3">
+                            <a class="thumbnail" id="carousel-selector-15"><img src="http://placehold.it/255x152&text=15"></a>
+                        </li>
+                    </ul>
+                </div>
+                <!--/Slider-->
+            </div>
+
+        </div>
+    </div><!--container-->
+
+
+    <div class="item_modal">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6 item_detail_pic">
+            <a class="thumbnail" id="carousel-selector-15"><img src="http://placehold.it/1000x500&text=15"></a>
+          </div>
+          <div class="col-sm-12 col-md-6 item_detail_data">
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  </body>
+</html>
